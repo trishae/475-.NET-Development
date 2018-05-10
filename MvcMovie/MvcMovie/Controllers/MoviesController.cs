7cfe2d054952d686d860f408810d96de98e5e2ee
Trisha Echual <piechual@gmail.com>
2018-05-10 15:53:16 -0700

    Bind Rating Property
    Missing in MvcMovie

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -128,7 +128,7 @@ namespace MvcMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.ID)
             {
