b58d2c8cae70553bd891db7ba2a7e77acd8da673
Trisha Echual <piechual@gmail.com>
2018-05-10 18:20:09 -0700

    Unit Testing Demo for basic MVC project

---
+++ MVCUnitTestingDemo/MVCUnitTestingDemo.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCUnitTestingDemo.Controllers;
+using System.Web.Mvc;
+
+[TestClass]
+public class EmployeeControllerTest
+{
+    [TestMethod]
+    public void Employees()
+    {
+        // Arrange
+        EmployeeController controller = new EmployeeController();
+
+        // Act
+        ViewResult result = controller.Index() as ViewResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+    }
+}
